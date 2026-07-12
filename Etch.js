8d818122e5b87d9e6dda6f1b914f58966e395bff
Andrei Mago <alexandrei.mago1@gmail.com>
2026-07-12 20:55:06 +0800

    Fixed the paint bug. it wasn't painting anything and also adhered to the instructions about doing the change size button that prompts instead of a slider. it's good but i still wanna upgrade it in the future

--- Etch.js
+++ Etch.js
@@ -1,5 +1,5 @@
 //Variables
-let mySlider = document.getElementById('sizeSlider')
+let sizePromptBtn = document.getElementById('sizePromptBtn')
 let myNewGridBtn = document.getElementById('newGridBtn')
 let myContainer = document.getElementById('gridContainer')
 let sizeDisplay = document.getElementById('sizeValue')
@@ -10,30 +10,34 @@ let isDrawing = false;
 // Grid Size 
 function gridSizeChanger(size){
     myContainer.innerHTML = '';
-    myContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    myContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
-
-     for (let i = 0; i < size * size ; i++){
-        const cell = document.createElement('div')
-        cell.classList.add('cell')
-
-        cell.addEventListener('mousedown', () => {
-            isDrawing = true;
-            cell.classList.add('active');
-        });
-
-        cell.addEventListener('mousedown', () => {
-            if (isDrawing) {
-                cell.classList.add('active');
+  
+    const cellSize = 400/size;
+    for(let i = 0; i < size * size  ; i++ ){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.width = `${cellSize}px`;
+         cell.style.height = `${cellSize}px`;
+         cell.addEventListener('mousedown', () => paintCell(cell));
+        cell.addEventListener('mouseover', (e) => {
+            if (e.buttons === 1) { // Checks if left mouse is held down
+                paintCell(cell);
             }
         });
 
-        myContainer.appendChild(cell)
-     }
+         myContainer.appendChild(cell)
+    }
 }
-// Slider Function
-mySlider.addEventListener('input', (e) => {
-    sizeDisplay.textContent = e.target.value;
+
+// Size Prompt
+
+sizePromptBtn.addEventListener('click',() => {
+    let newSize = prompt("Enter number of squares per side(max of 100)");
+    newSize = parseInt(newSize)
+    if (newSize > 0 && newSize <= 100){
+        gridSizeChanger(newSize)
+    }else {
+        return alert("Please Enter a number between 1 and 100")
+    }
 })
 
 // Grid Btn
@@ -54,3 +58,13 @@ function resetGrid(){
 myResetBtn.addEventListener('click', () => {
     resetGrid()
 })
+//RandomColor
+function getRandomColor(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`
+}
+function paintCell(cell){
+    cell.style.backgroundColor = getRandomColor()
+}
